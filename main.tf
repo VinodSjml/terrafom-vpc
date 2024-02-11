325e50425718289cfd1d128cda03cf83baf12215
vinod sjml <vinsjml@gmail.com>
2024-02-11 18:44:45 +0530

    push

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 module "vpc"{
-    source   = "/home/centos/terraform-vpc/vendor/modules/vpc"
-    VPC_CIDR = var.VPC_CIDR
+    source             = "/home/centos/terraform-vpc/vendor/modules/vpc"
+    VPC_CIDR           = var.VPC_CIDR
+    AZ                 = var.AZ
+    ENV                = var.ENV
+    PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+    PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR 
 }
