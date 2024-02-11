7e461e099fa7be75ec6d092f04679b11cc9eca32
vinod sjml <vinsjml@gmail.com>
2024-02-11 21:40:24 +0530

    push

--- main.tf
+++ main.tf
@@ -5,4 +5,5 @@ module "vpc"{
     ENV                = var.ENV
     PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
     PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR 
+    DEFAULT_VPC_ID     = var.DEFAULT_VPC_ID
 }
