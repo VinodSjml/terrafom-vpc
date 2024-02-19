a76492b6432282daa0839b9bc2d54ac76a72e2ea
vinod sjml <vinsjml@gmail.com>
2024-02-19 11:12:24 +0530

    push

--- variables.tf
+++ variables.tf
@@ -10,5 +10,5 @@ variable "DEFAULT_VPC_RT" {}
 variable "PRIVATE_HOSTED_ZONE_ID" {}
 variable "PRIVATE_HOSTED_ZONE_NAME" {}
 
-# variable "PUBLIC_HOSTED_ZONE_ID" {}
-# variable "PUBLIC_HOSTED_ZONE_NAME" {}
+variable "PUBLIC_HOSTED_ZONE_ID" {}
+variable "PUBLIC_HOSTED_ZONE_NAME" {}
